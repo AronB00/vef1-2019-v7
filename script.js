c7fda06f22f9860ab5e13de3ea0c9f26987d8a4e
AronB00 <abk18@hi.is>
2019-10-30 12:58:55 +0000

    fifth commit

--- script.js
+++ script.js
@@ -14,47 +14,16 @@
  */
 
  const games = [];
- 
-
-
- /**
-  * Byrjar leikinn okkar með því að kalla í play().
-  * Eftir að play() klárar þá er boðið notandanum að spila annann leik með confirm()
-  * Ef notandi ýtir á "ok" þá er annar leikur spilaður.
-  * Ef notandi ýtir á "cancel" þá er sótt niðurstöður með getResults() og þær birtar með alert().
-  */
-function start() {
-  play();
-  if(confirm('viltu spila aftur') == true){
-    games[leikir] = tilraunir;
-    leikir ++;
-    getResults(random, 0, false, 0);
-  }else{
-    for(var i = 0; i < leikir; i++){
-      alert('Leikur nr: ' + games[i]);
-    }
-  }
-}
+ var leikir = 0;
 
 /**
- * Spilar einn leik. Sér um að:
- *  - Velja tölu af handahófi í byrjun með randomNumber()
- *  - Biðja notanda um tölu með prompt()
- *  - Vinna úr intaki frá notanda með parseGuess()
- *  - Láta vita hversu nálægt eða rétt gisk er með getResponse() og alert()
- *  - Haldautan um fjölda ágiskana
- *  - Vista fjölda ágiskana í "games" fylki þegar búið er að giska rétt
- * 
- * Ef notandi ýtir á cancel þegar beðið er um ágiskun skal hætta í leik en ekki vista ágiskanir
- *  - t.d. með því að nota break í lykkju.
- * 
- * Þarf aðútfæra með lykkju og flæðisstýringum
+ * Spilar einn leik.
  */
 function play() {
   const random = randomNumber(1,100);
-  var gisk = 0;
-  var rett = false;
-  var tilraunir = 0;
+  const gisk = 0;
+  const rett = false;
+  const tilraunir = 0;
   
   getResults(random, gisk, rett, tilraunir);
 
@@ -62,18 +31,27 @@ function play() {
 }
 
 /**
- * Skilar niðurstöðum um spilaða leiki sem streng.
- * Fjöldi liekja er skilað ásamt meðalfjölda giska, t.d.:
- *    "þú spilaðir 10 leiki
- *     Meðalfjöldi ágiskana var 5"
- * ATH að meðalfjöldi kemur í nýrri línu.
- * Ef enginn leikur var spilaður er skilað:
- *    "Þú spilaðir engann leik >_<"
+ * Tekur inn ágiskun, gáir hvort að það sé tala.
+ * Ef svo er þá tékkar það á hversu langt frá réttu töluni ágiskunin er og prentar út eftirfarandi:
+ * ef ekki tala "Ekki tala!"
+ * ef gisk < 0 "Ekki rétt!"
+ * ef gisk er rétt "rétt!"
+ * ef munur < 5 "Mjög nálægt"
+ * ef munur < 10 "Nálægt"
+ * ef munur < 20 "Frekar langt frá"
+ * ef munur < 50 "Langt frá"
+ * annars "Mjög langt frá"
+ * bætir svo fjölda tilrauna inn í games.
  */
 function getResults(random, gisk, rett, tilraunir){
   do{
     gisk = prompt('Giskaðu á tölu milli 0 - 100: ');
-    if(gisk < 0){
+    if(gisk === null){
+      spilaAftur(tilraunir);
+    }else if(gisk != Number(gisk)){
+      isNaN(gisk);
+      alert('Ekki tala!')
+    }else if(gisk < 0){
       alert('Ekki rétt!');
     }else if(gisk == random){
       rett = true;
@@ -91,69 +69,25 @@ function getResults(random, gisk, rett, tilraunir){
     }
 
     tilraunir ++;
-    
-
   }while(rett == false);
 
-  return tilraunir;
+  Games = games.push(tilraunir);
+
+  spilaAftur(tilraunir);
 
 }
 
 /**
- * Reiknar út og skilar meðal ágiskunum í öllum leikjum sem geymdir eru í 
- * global breytu "games". Skilar gildi með tveim aukastöfum.
- * Ef games = [3,3,4] er niðurstaðan (3+3+4)/3 = 3.66666667
- * og henni skilað sem 3.67
- * 
- * þarf að útfæra með lykkju.
+ * Spyr að leik loknum hvort að þú viljir spila annan leik,
+ * ef svo er þá runnar það aftur play();
+ * annars segir það þér hversu marga leiki þú spilaðir og hver meðalfjöldi ágiskana var.
  */
 function spilaAftur(){
-
-}
-
-/**
- * tekur in input sem streng og skilar þeirri tölu sem hægt er að ná þar úr.
- * Ef ekki er hægt að ná tölu úr input er skilað null
- */
-function parseGuess(input){
-  input = prompt('Giskaðu á tölu');
-  getResponse(input, random);
-}
-
-/**
- * Skilar svari sem birta á notanda sem streng, tekur inn tvær breytur
- *  - guess sem tölu, ágiskun notanda
- *  - correct sem tölu, rétt gildi
- * Ef guess er < 0 eða ekki tala skal skila strengnum "Ekki rétt"
- * Ef guess er nákvæmlega sama og correct skal skila strengnum "Rétt"
- * Ef munur er undir 5 (|correct - guess| < 5) skal skila "Mjög nálægt"
- * Ef munur er undir 10 skal skila "Nálægt"
- * Ef munur er undir 20 skal skila "Frekar langt frá"
- * Ef munur er undir 50 skal skila "Langt frá"
- * Annars skal skila "Mjög langt frá"
- * 
- * Þarf að útfæra með flæðistýringu.
- * Math.abs skilar algildi tölu: |a| = Math.abs(a)
- */
-function getResponse(guess, correct){
-  if(guess < 0){
-    alarm('Ekki rétt');
-    play();
-  }else if(Math.abs(guess-correct) < 5){
-    alarm('Mjög nálægt');
-    play();
-  }else if(Math.abs(guess-correct) < 10){
-    alarm('Nálægt');
-    play();
-  }else if(Math.abs(guess-correct) < 20){
-    alarm('Frekar langt frá');
-    play();
-  }else if(Math.abs(guess-correct) < 50){
-    alarm('Langt frá');
+  const SpilaAftur = confirm('Viltu spila aftur?');
+  if(SpilaAftur == true){
     play();
   }else{
-    alarm('Mjög langt frá');
-    play();
+    alert('þú spilaðir ' + games.length + ' leiki \n'+ 'Meðalfjöldi ágiska var ' + (games.reduce((a,b) => a + b, 0)/parseInt(games.length)));
   }
 }
 
@@ -165,4 +99,4 @@ function randomNumber(min, max) {
 }
 
 // Byrjar leik
-start();
+play();
